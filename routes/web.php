755bf710cb79a5a39f2e68926262b7d31a281e66
zack <javck.zack@gmail.com>
2021-10-08 18:47:08 +0800

    完成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,9 +12,34 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/','welcome');
+
+//Route::get('contact','App\Http\Controllers\SiteController@contact');
+Route::get('contact',[SiteController::class,'contact']);
+
+// Route::get('user/{id}',function($id){
+//     return 'User' . $id;
+// });
+
+Route::prefix('demo')->group(function(){
+    Route::get('hello',function(){
+        return 'Hello';
+    });
+    Route::get('world',function(){
+        return 'World';
+    });
 });
 
+Route::get('user/{user}', function(User $user){
+    dd($user);
+});
 
+Route::get('redir',function(){
+    return redirect(url('demo/hello'));
+});
