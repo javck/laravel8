2c9786e16845c5ff784895ceb5a25678b290af22
zack <javck.zack@gmail.com>
2021-10-08 17:43:22 +0800

    完成

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,61 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//條件判斷式示範區=====================
+Route::get('if',function(){
+    $age = 16;
+
+    if( $age > 18){
+        dd('你可以開車囉!');
+    }
+});
+
+Route::get('/else',function(){
+    $name = 'Edgar';
+
+    if( $name == 'Simon'){
+        dd('我認識你');
+    }else{
+        dd('你是誰');
+    }
+});
+
+Route::get('/elseif',function(){
+    $name = 'Mary';
+
+    if ($name == 'Simon') {
+        dd('我認識你');
+    }elseif($name == 'Mary'){
+        dd('我聽過你');
+    }else{
+        dd('你是誰');
+    }
+});
+
+Route::get('/switch',function(){
+    $myNum = 3;
+    switch( $myNum){
+        case 1:
+            dd(1);
+            break;
+        case 2:
+            dd(2);
+            break;
+        default:
+            dd('以上皆非');
+    }
+});
+
+Route::get('/switch2',function(){
+    $myNum = 2;
+    switch( $myNum){
+        case 1:
+        case 2:
+            dd('1或2');
+            break;
+        default:
+            dd('以上皆非');
+    }
+});
+//==========================================
 
