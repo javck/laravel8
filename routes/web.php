ec05ddeb414d460bdd972277a28ce8f940f5ad64
zack <javck.zack@gmail.com>
2021-10-08 17:56:48 +0800

    完成

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('array',function(){
+    //$box = array('玩具','衣服','文具','雜物');
+    $box = ['玩具','衣服','文具','雜物'];
+    //dd($box);
 
+    //dd(count($box));
+
+    //dd($box[1]);
+
+    $box[1] = '手機';
+    //dd($box[1]);
+
+    //unset($box[1]);
+    //dd($box[1]);
+    //dd(count($box));
+
+    // unset($box);
+    // dd(count($box));
+
+});
+
+Route::get('array2',function(){
+    $box = ['玩具',['襪子','鞋子'],'文具','雜物'];
+
+    dd($box[1][0]);
+});
+
+Route::get('array3',function(){
+    // $hero = ['Zack', 100 , 300 ];
+    // $hp = $hero[1];
+    $hero = [ 'name' => 'Zack' , 'hp' => 100 , 'mp' => 300];
+    dd($hero['hp']);
+});
