2524206eb43ab90170ecaa10c62b88ac3efb4e2c
zack <javck.zack@gmail.com>
2021-11-23 21:25:47 +0800

    Seeder示範完成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Eloquent;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        Eloquent::unguard();
+        $this->call([
+            TaskSeeder::class
+        ]);
+        Eloquent::reguard();
+
+
     }
 }
